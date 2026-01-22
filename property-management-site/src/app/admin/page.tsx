464f9cf089da6da762f3186ff10c12c16a9fd125
AtanasRaev <atanaspraev@gmail.com>
2026-01-22 17:15:36 +0200

    Add admin homepage with basic layout and management overview

---
+++ property-management-site/src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminHome() {
+    return (
+        <div style={{ padding: 20 }}>
+            <h1>Admin</h1>
+            <p>From here you will manage blocks, apartments, payments.</p>
+        </div>
+    );
+}
