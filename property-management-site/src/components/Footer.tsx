ea5cf4f266ac46a9400bfa83c414db3162333070
emilbankov <emilbankov17d@gmail.com>
2026-01-23 17:04:15 +0200

    Added footer

---
+++ property-management-site/src/components/Footer.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-2 mb-4">
+            <div className="footer-link">
+              <h3 className="footer-heading">Get Started</h3>
+              <ul className="foolter-link-list">
+                <li className="foolter-link-item">
+                  <a href="#">Visit AssetNest</a>
+                </li>
+                <li className="foolter-link-item">
+                  <a href="#">Apply Now</a>
+                </li>
+                <li className="foolter-link-item">
+                  <a href="#">View tution and Aid</a>
+                </li>
+                <li className="foolter-link-item">
+                  <a href="#">Work at AssetNest</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-2 mb-4">
+            <div className="footer-link">
+              <h3 className="footer-heading">Discover</h3>
+              <ul className="foolter-link-list">
+                <li className="foolter-link-item">
+                  <a href="#">Visit AssetNest</a>
+                </li>
+                <li className="foolter-link-item">
+                  <a href="#">Apply Now</a>
+                </li>
+                <li className="foolter-link-item">
+                  <a href="#">View tution and Aid</a>
+                </li>
+                <li className="foolter-link-item">
+                  <a href="#">Work at AssetNest</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 mb-4">
+            <div className="footer-social">
+              <h3 className="footer-heading">Social</h3>
+              <ul className="foolter-social-list">
+                <li className="foolter-link-item">
+                  <a href="#">
+                    <i className="fa fa-facebook" />
+                  </a>
+                </li>
+                <li className="foolter-link-item">
+                  <a href="#">
+                    <i className="fa-brands fa-x-twitter" />
+                  </a>
+                </li>
+                <li className="foolter-link-item">
+                  <a href="#">
+                    <i className="fa fa-instagram" />
+                  </a>
+                </li>
+                <li className="foolter-link-item">
+                  <a href="#">
+                    <i className="fa fa-linkedin" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-2 mb-4">
+            <div className="footer-link">
+              <h3 className="footer-heading">Location</h3>
+              <ul className="foolter-link-list">
+                <li className="foolter-link-item">
+                  <a href="#">1234, New Winston Road, New York, NY10021</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-3 mb-4">
+            <div className="footer-contact-btns">
+              <ul>
+                <li>
+                  <a href="mailto:info@example.com">
+                    <i className="fa fa-envelope" />
+                    info@example.com
+                  </a>
+                </li>
+                <li>
+                  <a href="tel:+1234567890">
+                    <i className="fa fa-phone" />
+                    +1234567890
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-map-marker" />
+                    Maps &amp; Directions
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-12 col-lg-12">
+            <div className="footer-middle-links">
+              <ul>
+                <li>
+                  <a href="home">Home</a>
+                </li>
+                <li>
+                  <a href="terms-of-use">Terms &amp; Conditions</a>
+                </li>
+                <li>
+                  <a href="privacy-policy">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="#">Website Feedback</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-12 col-lg-12">
+            <div className="footer-bottom">
+              <p>All rights reserved @AssetNest.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
