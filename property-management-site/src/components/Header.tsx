f3b303d59f8f3e035c07e63c3647169457491b8b
emilbankov <emilbankov17d@gmail.com>
2026-01-23 17:04:09 +0200

    Added header

---
+++ property-management-site/src/components/Header.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+export default function Header() {
+  return (
+    <header>
+      <nav id="navbar_top" className="navbar navbar-expand-lg navbar-dark">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            <img
+              loading="lazy"
+              className="img-fluid"
+              src="/images/logo.png"
+              alt="img"
+            />
+            {/* AssetNest */}
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#main_nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse home" id="main_nav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/pages/about-us">
+                  {" "}
+                  About{" "}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/pages/plans">
+                  {" "}
+                  Plans{" "}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/pages/services">
+                  {" "}
+                  Services{" "}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/pages/properties">
+                  {" "}
+                  Properties{" "}
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link"
+                  href="#"
+                  id="resources"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Resources{" "}
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="resources">
+                  <li>
+                    <a className="dropdown-item" href="/pages/blog">
+                      Blog
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/pages/faq">
+                      FAQ
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/pages/testimonial">
+                      Testimonial
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/pages/error">
+                      Error 404
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/pages/coming-soon">
+                      Coming Soon
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="dropdown-item"
+                      href="/pages/privacy-policy"
+                    >
+                      Privacy Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="dropdown-item"
+                      href="/pages/terms-and-conditions"
+                    >
+                      Terms &amp; Conditions
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/pages/contact-us">
+                  {" "}
+                  Contact{" "}
+                </a>
+              </li>
+            </ul>
+            <div className="d-flex align-items-center">
+              <a className="theme-btn btn-white" href="/pages/contact-us">
+                Post Property <span className="pp-header-info">FREE</span>
+              </a>
+              <a className="header-contact-btn" href="#">
+                <i className="fa fa-phone" />
+              </a>
+              <a className="header-contact-btn" href="#">
+                <i className="fa fa-user" />
+              </a>
+            </div>
+          </div>{" "}
+          {/* navbar-collapse.// */}
+        </div>{" "}
+        {/* container-fluid.// */}
+      </nav>
+    </header>
+  );
+}
